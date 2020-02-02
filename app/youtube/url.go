8e431e89ee4cdfe877d20ff9d4ef07ec52794d54
qianjianeng <qianjianeng@easestar.cn>
2020-02-02 15:39:01 +0800

    update

---
+++ app/youtube/url.go
@@ -0,0 +1,20 @@
+package youtube
+
+import (
+	"myProject/videoCollector/collector"
+)
+
+func (e *Engine) FetchUrlVideos(urls []string, collector *collector.Collector) {
+
+	if len(urls) == 0 {
+		return
+	}
+
+	videos := e.getVideosByIds(urls)
+	for _,v := range videos {
+		v.Pass = true
+	}
+	if len(videos) > 0 {
+		collector.PushVideos(videos)
+	}
+}
